39fbea7410fbab8c2fce65f154be9eef017a428e
harshanricse <tunuguntlaharsha@gmail.com>
2026-04-26 15:26:46 +0530

    creating and reading signal in product-selection component

--- apm-begin/src/app/products/product-selection/product-selection.ts
+++ apm-begin/src/app/products/product-selection/product-selection.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-selection',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './product-selection.html',
   styleUrl: './product-selection.css'
 })
 export class ProductSelection {
   pageTitle = 'Product Selection';
+
+  quantity = signal(1);
 
 }
