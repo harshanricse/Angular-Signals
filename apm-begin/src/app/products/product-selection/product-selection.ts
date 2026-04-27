492032f1ee8303a1f960764c9d0afc7eed0d435b
harshanricse <tunuguntlaharsha@gmail.com>
2026-04-27 07:25:51 +0530

    retrieving data into the products signal with httpResource()

--- apm-begin/src/app/products/product-selection/product-selection.ts
+++ apm-begin/src/app/products/product-selection/product-selection.ts
@@ -1,8 +1,8 @@
-import { Component, computed, effect, linkedSignal, signal } from '@angular/core';
+import { Component, computed, effect, inject, linkedSignal, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ProductData } from '../product-data';
 import { Product } from '../product';
 import { CurrencyPipe } from '@angular/common';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-selection',
@@ -12,6 +12,7 @@ import { CurrencyPipe } from '@angular/common';
 })
 export class ProductSelection {
   pageTitle = 'Product Selection';
+  private productService = inject(ProductService)
 
   selectedProduct = signal<Product | undefined>(undefined);
   quantity = linkedSignal({
@@ -19,7 +20,7 @@ export class ProductSelection {
     computation: p => 1
   });
 
-  products = signal(ProductData.products);
+  products = this.productService.productsResource.value;
 
   total = computed(()=> (this.selectedProduct()?.price ?? 0) * this.quantity());
   color = computed(()=> this.total() >= 200 ? 'green' : 'blue')
